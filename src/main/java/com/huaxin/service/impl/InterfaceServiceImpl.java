897d966e1453a26240c5d4cf633a99b07e4d1b81
cenlongwu <71925446@qq.com>
2019-06-21 14:33:57 +0800

    整合接口模块                   ---6.21

---
+++ src/main/java/com/huaxin/service/impl/InterfaceServiceImpl.java
@@ -0,0 +1,21 @@
+package com.huaxin.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.huaxin.dao.InterfaceDao;
+import com.huaxin.entity.Interface;
+import com.huaxin.service.InterfaceService;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author zhangyao
+ * @since 2019-06-17
+ */
+@Service
+public class InterfaceServiceImpl extends ServiceImpl<InterfaceDao, Interface> implements InterfaceService {
+
+}
