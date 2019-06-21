d41041a79c01dc2b46412807324636c73ea84cb1
cenlongwu <71925446@qq.com>
2019-06-21 16:29:18 +0800

    整合接口模块                   ---6.21

---
+++ src/main/java/com/huaxin/dao/InterfaceDao.java
@@ -0,0 +1,19 @@
+package com.huaxin.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.huaxin.entity.Interface;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zhangyao
+ * @since 2019-06-17
+ */
+@Mapper
+public interface InterfaceDao extends BaseMapper<Interface> {
+
+}
