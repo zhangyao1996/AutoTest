9b765da5c6dc2e2906ea97821f6daf8d7fe54add
cenlongwu <71925446@qq.com>
2019-06-21 14:31:14 +0800

    整合接口模块                   ---6.21

---
+++ src/main/java/com/huaxin/service/InterfaceService.java
@@ -0,0 +1,16 @@
+package com.huaxin.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.huaxin.entity.Interface;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zhangyao
+ * @since 2019-06-17
+ */
+public interface InterfaceService extends IService<Interface> {
+
+}
